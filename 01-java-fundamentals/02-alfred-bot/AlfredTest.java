5d3bb62f8f1e9b8e344587a8dee722ef4e9a6f01
trietthai <90355157+trietthai@users.noreply.github.com>
2021-09-28 18:15:24 -0700

    Update AlfredTest.java
    initial commit

--- 01-java-fundamentals/02-alfred-bot/AlfredTest.java
+++ 01-java-fundamentals/02-alfred-bot/AlfredTest.java
@@ -16,12 +16,6 @@ public class AlfredTest
         String alexisTest = alfredBot.respondBeforeAlexis("Alexis! Play some low-fi beats.");
         String alfredTest = alfredBot.respondBeforeAlexis("I can't find my yo-yo. Maybe Alfred will know where it is.");
         String notRelevantTest = alfredBot.respondBeforeAlexis("Maybe that's what Batman is about. Not winning. But failing..");
-        // Print the greetings to test.
-        System.out.println(testGreeting);
-        System.out.println(testGuestGreeting);
-        System.out.println(testDateAnnouncement);
-        System.out.println(alexisTest);
-        System.out.println(alfredTest);
-        System.out.println(notRelevantTest);
+
     }
 }
