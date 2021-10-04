04e9c084c17fd250e489f8a2a01846f5390cb40e
trietthai <triet25@hotmail.com>
2021-10-04 16:58:21 -0700

    initial commit

--- 02-java-oop/03-zoo-keeper-pt1/GorillaTest.java
+++ 02-java-oop/03-zoo-keeper-pt1/GorillaTest.java
@@ -7,7 +7,13 @@ public class GorillaTest
     public static void main(String[] args)
     {
         Gorilla gorilla = new Gorilla();
-        gorilla.throwSomething().eatBananas().climb();
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.throwSomething();
+        gorilla.eatBananas();
+        gorilla.eatBananas();
+        gorilla.climb();;
+      
 
 
     }
