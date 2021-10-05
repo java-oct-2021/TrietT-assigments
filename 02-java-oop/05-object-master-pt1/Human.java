d98c3c4e03ee49f2cbb5eff82e47543ce2e0c8c2
trietthai <triet25@hotmail.com>
2021-10-05 11:46:48 -0700

    initial commit

---
+++ 02-java-oop/05-object-master-pt1/Human.java
@@ -0,0 +1,50 @@
+public class Human 
+{
+    //Attributes / File
+    private int strength =3;
+    private int stealth =3;
+    private int intelligence =3;
+    private int health =100;
+
+    public int getStrength()
+    {
+        return strength;
+    }
+    public void setStrength(int strengthNum)
+    {
+        strength = strengthNum;
+    }
+
+    public int getStealth()
+    {
+        return stealth;
+    }
+    public void setStealth(int stealthNum)
+    {
+        stealth = stealthNum;
+    }
+
+    public int getIntelligence()
+    {
+        return intelligence;
+    }
+    public void setIntelligence(int intelligenceNum)
+    {
+        intelligence = intelligenceNum;
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+    public void setHealth(int healthNum)
+    {
+        health = healthNum;
+    }
+
+    public void attackHuman()
+    {
+        
+    }
+
+}
