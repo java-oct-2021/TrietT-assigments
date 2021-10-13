3c54e03373bbb974389d304b811f20ec5a9692f1
trietthai <90355157+trietthai@users.noreply.github.com>
2021-10-12 17:36:10 -0700

    initial comnit

---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/hello-human/src/main/java/com/example/demo/human.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class human {
+	@RequestMapping("/")
+	public String index(@RequestParam(value="name", required=false) String searchQuery,  Model model) {
+        if(searchQuery != null){
+//            model.addAttribute("name", searchQuery);
+            return "NewFile.jsp";
+        }
+        else
+        	model.addAttribute("name", "Human");
+        	return "NewFile.jsp";
+	}
+	
+
+}
---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/hello-human/target/classes/com/example/demo/NewFile.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Hello Human</title>
+</head>
+<body>
+	<h1>Hello <c:out value="${name}"/>!</h1>
+	<h2>Welcome to SpringBoot!</h2>
+</body>
+</html>
