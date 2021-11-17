fbc365288826bcfd961e0106d5d5f7d8084368d4
trietthai <90355157+trietthai@users.noreply.github.com>
2021-11-16 21:13:16 -0800

    login

--- 03-java-spring/05-login-and-registration/01-controller-and-views/authentication/src/main/java/authentication/services/UserService.java
+++ 03-java-spring/05-login-and-registration/01-controller-and-views/authentication/src/main/java/authentication/services/UserService.java
@@ -3,6 +3,7 @@ package authentication.services;
 import java.util.Optional;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import authentication.models.User;
@@ -12,50 +13,38 @@ import authentication.repositories.UserRepository;
 
 public class UserService {
 	
-private final UserRepository userRepository;
-    
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-    
-    // register user and hash their password
-    public User registerUser(User user) {
-        String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
-        user.setPassword(hashed);
-        return userRepository.save(user);
-    }
-    
-    // find user by email
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
-    
-    // find user by id
-    public User findUserById(Long id) {
-    	Optional<User> u = userRepository.findById(id);
-    	
-    	if(u.isPresent()) {
-            return u.get();
-    	} else {
-    	    return null;
-    	}
-    }
-    
-    // authenticate user
-    public boolean authenticateUser(String email, String password) {
-        // first find the user by email
-        User user = userRepository.findByEmail(email);
-        // if we can't find it by email, return false
-        if(user == null) {
-            return false;
-        } else {
-            // if the passwords match, return true, else, return false
-            if(BCrypt.checkpw(password, user.getPassword())) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
+	@Autowired
+	UserRepository userRepo;
+	
+	public User add (User user) {
+		String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+		user.setPassword(hashed);
+		return userRepo.save(user);
+	}
+	
+	public User getById(Long userId) {
+		return userRepo.findById(userId).get();
+	}
+	
+	public User getByEmail(String email) {
+		return userRepo.findByEmail(email);
+	}
+	
+	public boolean authenticatUser(String email, String password) {
+		User user = userRepo.findByEmail(email);
+		
+		if(user == null) {
+			System.out.println("user dne");
+			return false;
+		} else {
+			if(BCrypt.checkpw(password, user.getPassword())) {
+				System.out.println("authenticated!");
+				return true;
+			} else {
+				System.out.println("not authenticated!");
+				return false;
+			}
+		}
+	}
 
 }
